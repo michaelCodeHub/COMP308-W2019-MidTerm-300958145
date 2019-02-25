96f1f36e530962cce5663c4e6809c1808e62e519
Michael <michaeljaison.me@gmail.com>
2019-02-25 08:08:05 -0500

    DB Changed to MLab

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books308"
+  // "URI": "mongodb://localhost/books308"
   //Remote MongoDB deployment -> let 
-  //"URI" :"mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
+  "URI" :"mongodb://admin:admin123@ds137686.mlab.com:37686/books308"
 };
