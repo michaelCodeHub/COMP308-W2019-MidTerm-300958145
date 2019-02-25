680c8651efbab598a533443eb1ec1a1985e0c9b3
Michael <michaeljaison.me@gmail.com>
2019-02-25 11:13:04 -0500

    User Model Created

--- server/routes/books.js
+++ server/routes/books.js
@@ -12,8 +12,17 @@ let mongoose = require('mongoose');
 // define the book model
 let book = require('../models/books');
 
+
+function requireAuth(req, res, next) {
+  // check if the user is logged in
+  if(!req.isAuthenticated()) {
+      return res.redirect('/login');
+  }
+  next();
+}
+
 /* GET books List page. READ */
-router.get('/', (req, res, next) => {
+router.get('/', requireAuth, (req, res, next) => {
   // find all books in the books collection
   book.find( (err, books) => {
     if (err) {
@@ -30,7 +39,7 @@ router.get('/', (req, res, next) => {
 });
 
 //  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
+router.get('/add',requireAuth, (req, res, next) => {
     res.render('books/details', { 
       title: 'Add New Book',
       books: book
@@ -39,7 +48,7 @@ router.get('/add', (req, res, next) => {
 });
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+router.post('/add',requireAuth, (req, res, next) => {
   let newBook = book({
     "Title": req.body.title,
     "Description": req.body.description,
@@ -60,7 +69,7 @@ router.post('/add', (req, res, next) => {
 
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/:id',requireAuth, (req, res, next) => {
   let id = req.params.id;
   book.findById(id, (err, bookObject) => {
 
@@ -79,7 +88,7 @@ router.get('/:id', (req, res, next) => {
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/:id',requireAuth, (req, res, next) => {
 
   let id = req.params.id;
 
@@ -104,7 +113,7 @@ router.post('/:id', (req, res, next) => {
 });
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
+router.get('/delete/:id', requireAuth,(req, res, next) => {
   let id = req.params.id;
 
   book.remove( {_id:id} , (err, bookObject)=>{
--- server/routes/index.js
+++ server/routes/index.js
@@ -14,4 +14,35 @@ router.get('/', (req, res, next) => {
    });
 });
 
+
+/* GET - displays the Login Page */
+router.get('/login', (req, res, next)=>{
+if(!req.user){
+
+}else{
+  return res.redirect("/");
+}
+});
+
+/* POST - processes the Login Page */
+router.post('/login', (req, res, next)=>{
+  
+});
+
+/* GET - displays the User Registration Page */
+router.get('/register', (req, res, next)=>{
+  
+});
+
+/* POST - processes the User Registration Page */
+router.post('/register', (req, res, next)=>{
+  
+});
+
+/* GET - perform user logout */
+router.get('/logout', (req, res, next)=>{
+  
+});
+
+
 module.exports = router;
