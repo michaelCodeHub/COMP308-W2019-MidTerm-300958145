b401659412779c898db90ca2320038d92634a036
Michael <michaeljaison.me@gmail.com>
2019-02-25 07:59:18 -0500

    Edit and Delete functionality completed

--- server/routes/books.js
+++ server/routes/books.js
@@ -52,20 +52,48 @@ router.post('/add', (req, res, next) => {
 
 });
 
+
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+  book.findById(id, (err, bookObject) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+      if(err){
+          console.log(err);
+          res.end(err);
+      }
+      else{
+          console.log(bookObject);
+          res.render('books/details', { 
+              title: 'Edit Contact',
+              books : bookObject
+          });
+      }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  let updatedBook = book({
+      "_id": id,
+      "Title": req.body.title,
+      "Description": req.body.description,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+  });
+  book.update( {_id:id} , updatedBook, (err, bookObject)=>{
+      if(err){
+          console.log(err);
+          res.end(err);
+      }
+      else{
+          res.redirect("/books");
+      }
+  });
 
 });
 
